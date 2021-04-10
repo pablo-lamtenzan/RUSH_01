501e0f40d986f21bd7a61d6dfdd1c2eb2cd39c6e
Your Name <pablolamtenzan@gmail.com>
2021-04-10 02:24:53 +0200

    typedef for modular types

---
+++ includes/local_types.h
@@ -0,0 +1,15 @@
+/** @file types.h
+ *  
+ * @brief Local types definition.
+*/
+
+# pragma once
+
+# include <stdint.h> // fast & portable types
+
+/// Array operations
+typedef uint_fast32_t uint_op_t;
+/// Pointer operations
+typedef uint_fast64_t uint_size_t;
+/// Pointer arithmetic
+typedef int_fast64_t int_size_t;
