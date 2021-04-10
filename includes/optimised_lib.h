ec9524ff0a60fbda38c5d131031286d40a0e9ae6
Your Name <pablolamtenzan@gmail.com>
2021-04-10 02:28:52 +0200

    start of optimised lib

--- includes/optimised_lib.h
+++ includes/optimised_lib.h
@@ -5,4 +5,8 @@
 
 # include <local_types.h>
 
-inline void fast_swap(uint_op_t* const lhs, uint_op_t* const rhs);
+/* Swap integers */
+
+void op_swap(uint_op_t* const lhs, uint_op_t* const rhs);
+void u_size_swap(uint_size_t* const lhs, uint_size_t* const rhs);
+void s_size_swap(int_size_t* const lhs, int_size_t* const rhs);
