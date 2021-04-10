9ab44eaee4b187f54fef0881ca053dce215839ce
Your Name <pablolamtenzan@gmail.com>
2021-04-10 06:01:41 +0200

    square abstract representation & square creation & square write

--- includes/square.h
+++ includes/square.h
@@ -2,17 +2,41 @@
  * 
  *  A square can be stored in a @p uint_size_t.
  *  - 4 first bytes represent the border size.
- *  - 4 last bytes represent the last 4 bytes of the top left corner.
+ *  - 4 last bytes represent the last 4 bytes of the address of top left corner.
 */
 
 # pragma once
 
 # include <local_types.h>
 
+/** 
+ *  @brief struct used for print a square
+ *  @member: sq_left_corner: The address of the top left corner.
+ *  @member: sq_height: The height of the square.
+ *  @member: ascii: The ascii character used to fill the square.
+ *  @member: min_x: The address of start of the line.
+ *  @member: max_x: The address of the end of the line.
+ *  NOTE: size is 40 bytes.
+*/
+typedef struct  sq_print
+{
+    uint_op_t* const        sq_left_corner;
+    uint_size_t             sq_height;
+    uint_op_t               ascii;
+    struct
+    {
+        uint_op_t* const    min_x;
+        uint_op_t* const    max_x;
+    };
+}               sq_print_t;
+
 /// Prepend ( into @p pointer ) @p size, sizeof(sq_size_t) into @p pointer dereference (sizeof(uint_size_t))
 # define INJECT_SIZE(size, pointer) inject_append_4b_data(size, pointer)
-/// Repres
+/// Represent the size stored in @p pointer .
 # define EXTRACT_SIZE(pointer) ((uint_size_t)(pointer >> (sizeof(uint_size_t) / 2UL)))
-# define EXTRACT_ADDR(pointer, hint) inject_append_4b_data((sq_size_t)(hint >> (sizeof(uint_size_t) / 2UL), pointer)
+/// @p pointer takes the value of it original address.
+# define EXTRACT_ADDR(pointer, hint) inject_append_4b_data((sq_size_t)(hint >> (sizeof(uint_size_t) / 2UL), pointer))
 
-void inject_append_4b_data(sq_size_t data, uint_size_t**const pointer);
+void        inject_prepend_4b_data(sq_size_t data, uint_size_t*const pointer);
+sq_print_t  create_sq_print(uint_size_t sq, const uint_op_t* const min_x, const uint_op_t* const max_x, uint_op_t ascii, sq_size_t hint);
+void        write_sq(sq_print_t* const sq);
--- srcs/square.c
+++ srcs/square.c
@@ -10,10 +10,44 @@
  *  of @p pointer.
  *  @p pointer must be a reference to a @a uint_size_t .
 */
-inline void inject_append_4b_data(sq_size_t data, uint_size_t*const pointer)
+inline void
+inject_prepend_4b_data(sq_size_t data, uint_size_t*const pointer)
 {
     sq_size_swap(&data, (sq_size_t*const)pointer);
     *pointer <<= (sizeof(uint_size_t) / 2UL);
     sq_size_swap(&data, (sq_size_t*const)pointer);
 }
 
+# include <square.h>
+
+// If this is expensive just literally inline the code.
+inline sq_print_t
+create_sq_print(uint_size_t sq, const uint_op_t* const restrict min_x, const uint_op_t* const restrict max_x, uint_op_t ascii, sq_size_t hint)
+{    
+    sq_print_t obj = (sq_print_t){
+        .sq_height = EXTRACT_SIZE(sq),
+        .ascii = ascii,
+        .min_x = min_x,
+        .max_x = max_x
+    };
+    EXTRACT_ADDR((uint_size_t*const)&obj.sq_left_corner, hint);
+    return (obj);
+}
+
+/**
+ *  @brief writes the square @p sq in the matrix.
+ *  NOTE: All loops compare to 0 (the less expensive solution).
+*/
+void
+write_sq(sq_print_t* const sq)
+{
+    uint_size_t distance = (sq->max_x - sq->sq_left_corner) + ((sq->sq_left_corner + sq->sq_height) - sq->max_x);
+    uint_op_t* x = sq->sq_left_corner;
+
+    for (uint_size_t y = sq->sq_height ; y-- ;)
+    {
+        for (uint_size_t amount = sq->sq_height ; amount-- ;)
+            *(x + amount) = sq->ascii;
+        x += distance;
+    }
+}
