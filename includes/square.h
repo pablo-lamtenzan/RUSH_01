01d0be567d77169f8b389e47974b7c391d5f4673
Your Name <pablolamtenzan@gmail.com>
2021-04-12 00:24:23 +0200

    sq reprensentation struct

--- includes/square.h
+++ includes/square.h
@@ -30,6 +30,26 @@ typedef struct  sq_print
     };
 }               sq_print_t;
 
+/**
+ *  @brief Stores 2 pointer to @p uint_op_t .
+*/
+typedef struct  pair
+{
+    uint_op_t* const    y;
+    uint_op_t* const    x;
+}               t_pair;
+
+/**
+ * @brief Represent a square in the matrix.
+ * @member: coord, the position in the matrix of the %t_sq.
+ * @member: height the of the size.
+ * */
+typedef struct  sq
+{
+    uint_size_t height;
+    t_pair      coord;
+}               t_sq;
+
 /// Prepend ( into @p pointer ) @p size, sizeof(sq_size_t) into @p pointer dereference (sizeof(uint_size_t))
 # define INJECT_SIZE(size, pointer) inject_append_4b_data(size, pointer)
 /// Represent the size stored in @p pointer .
