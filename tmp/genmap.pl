fe70bfe0a800e33d8380eae4f0ee336391fd7cdf
Clément Hamada <clementhamada@protonmail.com>
2021-04-11 23:23:32 +0200

    Fix genmap.pl

--- tmp/genmap.pl
+++ tmp/genmap.pl
@@ -4,9 +4,11 @@ use strict;
 
 my $height = shift or die "Usage: $0 HEIGHT\n";
 
+print int($height), "\n.\n";
+
 for((1..$height)) {
     for ((1..$height)) {
-        print int(rand(10))
+        print int(rand(10));
     }
-    print "\n"
+    print "\n";
 }
