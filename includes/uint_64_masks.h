afda065d1aabc6433dddca34ab02474122de37a0
Your Name <pablolamtenzan@gmail.com>
2021-04-11 22:13:37 +0200

    comparison masks

---
+++ includes/uint_64_masks.h
@@ -0,0 +1,21 @@
+
+# pragma once
+
+/// 8 bytes alligned mask 0
+# define U64_0_MASK 0X0UL
+/// 8 bytes alligned mask 1
+# define U64_0_MASK 0X0101010101010101UL
+/// 8 bytes alligned mask 2
+# define U64_0_MASK 0X0202020202020202UL
+/// 8 bytes alligned mask 3
+# define U64_0_MASK 0X0303030303030303UL
+/// 8 bytes alligned mask 4
+# define U64_0_MASK 0X0404040404040404UL
+/// 8 bytes alligned mask 5
+# define U64_0_MASK 0X0505050505050505UL
+/// 8 bytes alligned mask 6
+# define U64_0_MASK 0X0606060606060606UL
+/// 8 bytes alligned mask 7
+# define U64_0_MASK 0X0707070707070707UL
+/// 8 bytes alligned mask 8
+# define U64_0_MASK 0X0808080808080808UL
