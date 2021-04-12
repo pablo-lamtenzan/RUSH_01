703a894b321bb089eabed784ff2c1cdb7d1fe8b9
Clément Hamada <clementhamada@protonmail.com>
2021-04-12 02:11:40 +0200

    Cleanup simple algo

--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,26 @@
+#include <unistd.h>
+#include <local_mem.h>
+#include <square.h>
 
-# include <local_mem.h>
-# include <square.h>
+uint_op_t   static_matrix[STATIC_ROOM]; // 8
+uint_op_t*  dynamic_matrix = NULL; // 8
+uint_size_t dynamic_lenght = 0; // 8
+uint_size_t height = 0; // 8
+uint_op_t   ascii = 0; // 4
+
+int find_the_square(t_sq *square);
+
+int print_matrix()
+{
+    return (write(STDOUT_FILENO, static_matrix, height * (height + 1)) != -1);
+}
 
 int main()
 {
     static t_sq sq = {0};
-    return (mem_read_stdin()
-    && find_the_square(&sq)
-    && insert_square(&sq)
-    && print_matrix());
+
+    return (!(mem_read_stdin()
+        && find_the_square(&sq)
+      //  && insert_square(&sq)
+        && print_matrix()));
 } 
