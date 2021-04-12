1f793e66432d06bb2e4d1d00a380f053b348b920
Your Name <pablolamtenzan@gmail.com>
2021-04-12 07:02:12 +0200

    invalid linking

--- srcs/main.c
+++ srcs/main.c
@@ -7,6 +7,6 @@ int main()
     static t_sq sq = {0};
     return (mem_read_stdin()
     && find_the_square(&sq)
-    && write_the_square(&sq)
+    && write_the_square(&sq, static_matrix)
     && print_matrix());
 } 
